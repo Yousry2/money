071fa9ad2c110051e1fcef61408f0d826484d5b0
Mohamed Yousry <mohammed.yousry@thoughtdesign.com.au>
2021-01-02 15:00:22 +0200

    US : basic architecture - add core models

--- src/app/shared/material/material.module.ts
+++ src/app/shared/material/material.module.ts
@@ -3,14 +3,18 @@ import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { MatTableModule } from '@angular/material/table';
 import {MatToolbarModule} from '@angular/material/toolbar';
-
+import {MatPaginatorModule} from '@angular/material/paginator';
+import {MatSortModule} from '@angular/material/sort';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    MatButtonModule, MatTableModule, MatToolbarModule
+    MatButtonModule, MatTableModule, MatToolbarModule ,  MatPaginatorModule,
+    MatSortModule,
+    MatProgressSpinnerModule
   ],
   exports: [MatButtonModule, MatTableModule, MatToolbarModule]
 })
