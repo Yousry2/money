0596ecbe144115fa138169df0adc707833292106
Mohamed Yousry <mohammed.yousry10@gmail.com>
2021-03-25 23:22:13 +0200

    develop - add angular material modules

---
+++ src/app/shared/material/material.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button'
+
+
+
+
+const MaterialModules = [MatButtonModule];
+@NgModule({
+  declarations: [],
+  imports: [MaterialModules
+  ],
+  exports: [MaterialModules]
+})
+export class MaterialModule { }
