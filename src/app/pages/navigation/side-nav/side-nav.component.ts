4805e19375ca2b58a6e2b6609eddfff2df84e67a
Mohamed Yousry <mohammed.yousry10@gmail.com>
2021-03-29 10:39:40 +0200

    develop: add styles to navigation menu

--- src/app/pages/navigation/side-nav/side-nav.component.ts
+++ src/app/pages/navigation/side-nav/side-nav.component.ts
@@ -3,6 +3,7 @@ import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
 import { PageSetupService } from '../../../core/basic/page-setup.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'money-side-nav',
@@ -19,12 +20,16 @@ export class SideNavComponent implements OnInit {
       shareReplay()
     );
 
-  constructor(private breakpointObserver: BreakpointObserver, private pageSetupService: PageSetupService) {
+  constructor(private breakpointObserver: BreakpointObserver, private pageSetupService: PageSetupService, private router: Router) {
     this.$pageName = this.pageSetupService.getPageName();
    }
 
   ngOnInit(): void {
   
+  }
+
+  goTo(link : string){
+    this.router.navigate([link]);
   }
 
 }
